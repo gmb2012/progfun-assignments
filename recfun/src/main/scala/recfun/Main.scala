2ea943ac5ce95315a0b229a43a290b0529049c3a
TAKANO Mitsuhiro <takano32@gmail.com>
2013-04-04 00:38:29 +0900

    solve `Exercise 2: Parentheses Balancing'
    https://class.coursera.org/progfun-002/assignment/view?assignment_id=4

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -29,8 +29,35 @@ object Main {
    * Exercise 2
    */
   def balance(chars: List[Char]): Boolean = {
-    false
+    def balance_r(parens: List[Char], chars: List[Char]): Boolean = {
+      chars.isEmpty match {
+        case true => parens.isEmpty
+        case false => {
+          val head = chars.head
+          val tail = chars.tail
+          head match {
+            case '(' => {
+                  balance_r('(' +: parens, tail)
+            }
+            case ')' => {
+              if (parens.isEmpty) return false
+              parens.head match {
+                case '(' => 
+                    balance_r(parens.tail, tail)
+                case _ => 
+                  false
+              }
+            }
+            case _ => {
+              balance_r(parens, tail)
+            }
+          }
+        }
+      }
+    }
+    balance_r(List(), chars)
   }
+        
 
   /**
    * Exercise 3
